625550c38bffe6478748644320b60968646f6743
hk <vaghaniharshkumar@gmail.com>
2025-07-21 12:42:54 +0530

    Add JavaScript for form submission interactivity

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example for form submission or other interactivity
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
